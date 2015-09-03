24a11a8a44377ae850d3216679253a8462103f2e
Andreas Schneider <asn@cryptomilk.org>
2015-09-03 11:04:00 +0200

    backuptool: Add checks if it makes sense to execute commands
    Change-Id: I73b1a360a290de1fe94380a01c2c7f3fd735f2ad

--- build/tools/backuptool.sh
+++ build/tools/backuptool.sh
@@ -13,7 +13,8 @@ cp -f /tmp/install/bin/backuptool.functions /tmp
 
 # Preserve /$S/addon.d in /tmp/addon.d
 preserve_addon_d() {
-  mkdir -p /tmp/addon.d/
+  if [ -d $S/addon.d/ ]; then
+    mkdir -p /tmp/addon.d/
     cp -a $S/addon.d/* /tmp/addon.d/
 
   # Discard any scripts that aren't at least our version level
@@ -27,20 +28,26 @@ preserve_addon_d() {
       fi
     done
 
-  chmod 755 /tmp/addon.d/*.sh
+    chmod 755 /tmp/addon.d/*.sh
+  fi
 }
 
 # Restore /$S/addon.d in /tmp/addon.d
 restore_addon_d() {
-  cp -a /tmp/addon.d/* $S/addon.d/
-  rm -rf /tmp/addon.d/
+  if [ -d /tmp/addon.d/ ]; then
+    mkdir -p $S/addon.d/
+    cp -a /tmp/addon.d/* $S/addon.d/
+    rm -rf /tmp/addon.d/
+  fi
 }
 
 # Execute /$S/addon.d/*.sh scripts with $1 parameter
 run_stage() {
-for script in $(find /tmp/addon.d/ -name '*.sh' |sort -n); do
-  $script $1
-done
+if [ -d /tmp/addon.d/ ]; then
+  for script in $(find /tmp/addon.d/ -name '*.sh' |sort -n); do
+    $script $1
+  done
+fi
 }
 
 case "$1" in
