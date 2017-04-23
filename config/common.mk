6f27ed4a1d209d161c11baae03381693a2eb6055
xplodwild <xplod@ouverta.fr>
2017-04-23 08:14:21 +0200

    Add basic vendor

---
+++ config/common.mk
@@ -0,0 +1,26 @@
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
+
+ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.com.google.clientidbase=android-google
+else
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.com.google.clientidbase=$(PRODUCT_GMS_CLIENTID_BASE)
+endif
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    keyguard.no_require_sim=true \
+    dalvik.vm.debug.alloc=0 \
+    ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
+    ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
+    ro.com.android.wifi-watchlist=GoogleGuest \
+    ro.setupwizard.enterprise_mode=1 \
+    ro.com.android.dateformat=MM-dd-yyyy \
+    ro.build.selinux=1
+
+PRODUCT_COPY_FILES += \
+    vendor/arrow/prebuilt/common/etc/init.d/00banner:system/etc/init.d/00banner \
+    vendor/arrow/prebuilt/common/bin/sysinit:system/bin/sysinit
+
+# Packages
+include vendor/arrow/config/packages.mk
---
+++ config/packages.mk
@@ -0,0 +1,13 @@
+# Required packages
+PRODUCT_PACKAGES += \
+    Basic \
+    CellBroadcastReceiver \
+    Development \
+    LatinIME
+
+# Extra tools
+PRODUCT_PACKAGES += \
+    openvpn \
+    e2fsck \
+    mke2fs \
+    tune2fs
