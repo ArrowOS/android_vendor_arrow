625eb1974fcf8173ac47f44ca004c29d49062f5d
cordell <cordellsmail@gmail.com>
2017-09-02 11:17:48 +0100

    World phone support
    Change-Id: Ibfc2ba0d58dd55f01d43435817e0cedf6f05d6bd

--- config/common.mk
+++ config/common.mk
@@ -30,6 +30,14 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/arrow/prebuilt/common/etc/init.local.rc:root/init.arrow.rc
 
+# World APN list
+PRODUCT_COPY_FILES += \
+    vendor/arrow/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
+
+# World SPN overrides list
+PRODUCT_COPY_FILES += \
+    vendor/arrow/prebuilt/common/etc/spn-conf.xml:system/etc/spn-conf.xml
+
 # Packages
 include vendor/arrow/config/packages.mk
 
