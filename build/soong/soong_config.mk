5b29425d13936bc51c16b2a5b2c8f89ebc916da5
Simon Shields <simon@lineageos.org>
2017-09-04 21:11:16 +1000

    build: soong: put arrow variables in the struct
    Change-Id: I9ed55cea0c55f0078f8f97ad4037b16ed11c0156

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -1,5 +1,8 @@
+# Insert new variables inside the Lineage structure
 arrow_soong:
 	$(hide) mkdir -p $(dir $@)
 	$(hide) (\
 	echo '{'; \
+	echo '"Arrow": {'; \
+	echo '},'; \
 	echo '') > $(SOONG_VARIABLES_TMP)
