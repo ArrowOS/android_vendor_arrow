fedc7d8529d54ff78dc8450f0c4493f53803aca3
Josh Chasky <jchasky@gmail.com>
2017-09-10 12:41:51 -0400

    Fix AOSP gesture typing
    Change-Id: Ia19043df237f41ebe0ff556fe794eb115d6ec303

--- config/common.mk
+++ config/common.mk
@@ -56,14 +56,14 @@ PRODUCT_COPY_FILES += \
     vendor/arrow/prebuilt/common/etc/sensitive_pn.xml:system/etc/sensitive_pn.xml
 
 # LatinIME gesture typing
-ifeq ($(TARGET_ARCH),arm64)
-PRODUCT_COPY_FILES += \
-    vendor/arrow/prebuilt/common/lib64/libjni_latinime.so:system/lib64/libjni_latinime.so \
-    vendor/arrow/prebuilt/common/lib64/libjni_latinimegoogle.so:system/lib64/libjni_latinimegoogle.so
-else
+ifneq ($(filter shamu,$(TARGET_PRODUCT)),)
 PRODUCT_COPY_FILES += \
     vendor/arrow/prebuilt/common/lib/libjni_latinime.so:system/lib/libjni_latinime.so \
     vendor/arrow/prebuilt/common/lib/libjni_latinimegoogle.so:system/lib/libjni_latinimegoogle.so
+else
+PRODUCT_COPY_FILES += \
+    vendor/arrow/prebuilt/common/lib64/libjni_latinime.so:system/lib64/libjni_latinime.so \
+    vendor/arrow/prebuilt/common/lib64/libjni_latinimegoogle.so:system/lib64/libjni_latinimegoogle.so
 endif
 
 # Pixel sysconfig
