db677c0e3b58af4153cab0bf5c79a1a7180c29d4
Carsten Hauge <carsten.hauge@sonymobile.com>
2017-10-17 15:49:16 -0400

    Import charging sounds
    Extracted from this commit
    https://android-review.googlesource.com/#/c/platform/frameworks/base/+/243420/
    
    Change-Id: Idba17bb1b6d2f6899bcd98d2fe7336d31835cb42

--- config/common.mk
+++ config/common.mk
@@ -46,6 +46,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/arrow/prebuilt/common/etc/sensitive_pn.xml:system/etc/sensitive_pn.xml
 
+# Charging sounds
+PRODUCT_COPY_FILES += \
+    vendor/arrow/prebuilt/common/media/audio/BatteryPlugged.ogg:system/media/audio/ui/BatteryPlugged.ogg \
+    vendor/arrow/prebuilt/common/media/audio/BatteryPlugged_48k.ogg:system/media/audio/ui/BatteryPlugged_48k.ogg
+
 # Packages
 include vendor/arrow/config/packages.mk
 
