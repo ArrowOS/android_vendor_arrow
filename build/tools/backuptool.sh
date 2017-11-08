240e1fabd4aa2edc00c08b158f040eb26b408ace
Warren Togami <wtogami@gmail.com>
2017-11-07 20:39:16 -0500

    Modular backuptool.sh. Executes backup and restore methods defined inarbitrary /system/addon.d/*.sh scripts
    Tips & Tricks
    =============
    * 50-cm.sh contains only a reference implementation.  You may customize the methods however you wish.
      For example, 20-foobar.sh pre-backup can use a loop with conditionals to generate a dynamic backup list in
      /tmp/foobar_file_list which is later printed by list_files() so the backup method will act on those files.
    * Optional methods pre-backup, post-backup, pre-restore, or post-restore may be defined for special purposes.
    * Inject new files into /tmp/addon.d/ prior to backuptool.sh backup if you want to act during the current CM upgrade.
    * Delete files from /tmp/addon.d/ during post-restore if you want to permanently remove files from /system/addon.d/
      Addons may use this approach to run a script only once.
    * Scripts run in sort -n order.  Prefix with numbers 00 through 99 if want to run in a particular order.
    * You can have two separate scripts, implementing only backup in one, and only restore in the other with a different
      number prefix of each.  This allows even greater control the backup/restore order even further.
    * You could use pre-backup to generate a one-time use backup script in /tmp/addon.d/ that deletes itself in
      post-restore.
    
    Patch Series
    ============
    http://review.cyanogenmod.com/#change,13265
    CyanogenMod/android_build
      * edify generator
    http://review.cyanogenmod.com/#change,13266
    CyanogenMod/android_system_core
      * permissions on /system/addon.d
    http://review.cyanogenmod.com/#change,13267
    CyanogenMod/android_vendor_cm
      * 50-cm.sh reference backup script
      * modular backuptool.sh
      * support backuptool.functions used by /system/addon.d/*.sh scripts
    
    Change-Id: Ifd5eaf9dcfd68d92e5043c21d1bae1dc0ad54860

--- build/tools/backuptool.sh
+++ build/tools/backuptool.sh
@@ -1,193 +1,68 @@
 #!/sbin/sh
 #
-# Backup and restore proprietary Android system files
+# Backup and restore addon /system files
 #
 
-C=/tmp/backupdir
-S=/system
-V=CyanogenMod-6
+export C=/tmp/backupdir
+export S=/system
 
-PROCEED=1;
-
-check_prereq() {
-   if ( ! grep -q "^ro.modversion=.*$V.*" /system/build.prop );
-   then
-      echo "Not backing up files from incompatible version.";
-      PROCEED=0;
-   fi
+# Mount /system if it is not already mounted
+mount_system() {
+if [ ! -f "$S/build.prop" ]; then
+  mount $S
+fi
 }
 
-check_installscript() {
-   if [ -f "/tmp/.installscript" ] && [ $PROCEED -ne 0 ];
-   then
-      # We have an install script, and ROM versions match!
-      # We now need to check and see if we have force_backup
-      # in either /etc or /tmp/backupdir 
-      if [ -f "$S/etc/force_backuptool" ] || [ -f "$C/force_backuptool" ];
-      then
-         echo "force_backuptool file found, Forcing backuptool."
-      else
-         echo "/tmp/.installscript found. Skipping backuptool."
-         PROCEED=0;
-      fi
-   fi
+# Unmount /system unless it is already unmounted
+umount_system() {
+if [ -f "$S/build.prop" ]; then
+  umount $S
+fi
 }
 
-get_files() {
-    cat <<EOF
-app/CarHomeGoogle.apk
-app/CarHomeLauncher.apk
-app/com.amazon.mp3.apk
-app/com.google.android.apps.maps-1.apk
-app/Facebook.apk
-app/GenieWidget.apk
-app/Gmail.apk
-app/GoogleBackupTransport.apk
-app/GoogleCalendarSyncAdapter.apk
-app/GoogleContactsSyncAdapter.apk
-app/GoogleFeedback.apk
-app/GoogleGoggles.apk
-app/GooglePartnerSetup.apk
-app/GoogleQuickSearchBox.apk
-app/GoogleServicesFramework.apk
-app/googlevoice.apk
-app/HtcCopyright.apk
-app/HtcEmailPolicy.apk
-app/HtcSettings.apk
-app/kickback.apk
-app/LatinImeGoogle.apk
-app/LatinImeTutorial.apk
-app/MarketUpdater.apk
-app/MediaUploader.apk
-app/NetworkLocation.apk
-app/OneTimeInitializer.apk
-app/PassionQuickOffice.apk
-app/SetupWizard.apk
-app/soundback.apk
-app/Street.apk
-app/Talk.apk
-app/talkback.apk
-app/Twitter.apk
-app/Vending.apk
-app/VoiceSearch.apk
-app/YouTube.apk
-etc/permissions/com.google.android.maps.xml
-framework/com.google.android.maps.jar
-lib/libinterstitial.so
-lib/libspeech.so
-etc/custom_backup_list.txt
-etc/force_backuptool
-EOF
+# Preserve /system/addon.d in /tmp/addon.d
+preserve_addon_d() {
+  mkdir -p /tmp/addon.d/
+  cp -a /system/addon.d/* /tmp/addon.d/
+  chmod 755 /tmp/addon.d/*.sh
 }
 
-get_custom_files() {
-   local L
-   if [ -f "$C/custom_backup_list.txt" ];
-   then
-      [ ! -f $C/fixed_custom_backup_list.txt ] && tr -d '\r' < $C/custom_backup_list.txt \
-            > $C/fixed_custom_backup_list.txt
-      L=`cat $C/fixed_custom_backup_list.txt`
-      cat <<EOF
-$L
-EOF
-   fi
+# Restore /system/addon.d in /tmp/addon.d
+restore_addon_d() {
+  cp -a /tmp/addon.d/* /system/addon.d/
+  rm -rf /tmp/addon.d/
 }
 
-backup_file() {
-   if [ -e "$1" ];
-   then
-      if [ -n "$2" ];
-      then
-         echo "$2  $1" | md5sum -c -
-         if [ $? -ne 0 ];
-         then
-            echo "MD5Sum check for $1 failed!";
-            exit $?;
-         fi
-      fi
-      
-      local F=`basename $1`
-      local D=`dirname $1`
-      
-      # dont backup any apps that have odex files, they are useless
-      if ( echo $F | grep -q "\.apk$" ) && [ -e `echo $1 | sed -e 's/\.apk$/\.odex/'` ];
-      then
-         echo "Skipping odexed apk $1";
-      else
-         mkdir -p $C/bak/$D
-         cp -p $1 $C/bak/$D/$F
-      fi
-   fi
+# Execute /system/addon.d/*.sh scripts with $1 parameter
+run_stage() {
+for script in $(find /tmp/addon.d/ -name '*.sh' |sort -n); do
+  $script $1
+done
 }
-
-restore_file() {
-   local FILE=`basename $1`
-   local DIR=`dirname $1`
-   if [ -e "$C/bak/$DIR/$FILE" ];
-   then
-      if [ ! -d "$DIR" ];
-      then
-         mkdir -p $DIR;
-      fi
-      cp -p $C/bak/$DIR/$FILE $1;
-      if [ -n "$2" ];
-      then
-         rm $2;
-      fi
-   fi
-}
-
-# don't (u)mount system if already done
-UMOUNT=0
 
 case "$1" in
-   backup)
-      if [ ! -f "$S/build.prop" ]; then
-         mount $S
-         UMOUNT=1
-      fi
-      check_prereq;
-      check_installscript;
-      if [ $PROCEED -ne 0 ];
-      then
-         rm -rf $C
-         mkdir -p $C
-         for file_list in get_files get_custom_files; do
-           $file_list | while read FILE REPLACEMENT; do
-              backup_file $S/$FILE
-           done
-         done
-      fi
-      if [ $UMOUNT -ne 0 ]; then
-         umount $S
-      fi
-   ;;
-   restore)
-      if [ ! -f "$S/build.prop" ]; then
-         mount $S
-         UMOUNT=1
-      fi
-      check_prereq;
-      check_installscript;
-      if [ $PROCEED -ne 0 ];
-      then
-         for file_list in get_files get_custom_files; do
-           $file_list | while read FILE REPLACEMENT; do
-              R=""
-              [ -n "$REPLACEMENT" ] && R="$S/$REPLACEMENT"
-              restore_file $S/$FILE $R
-           done
-         done
-         rm -rf $C
-      fi
-      if [ $UMOUNT -ne 0 ]; then
-         umount $S
-      fi
-      sync
-   ;;
-   *)
-      echo "Usage: $0 {backup|restore}"
-      exit 1
+  backup)
+    mkdir -p $C
+    mount_system
+    preserve_addon_d
+    run_stage pre-backup
+    run_stage backup
+    run_stage post-backup
+    umount_system
+  ;;
+  restore)
+    mount_system
+    run_stage pre-restore
+    run_stage restore
+    run_stage post-restore
+    restore_addon_d
+    umount_system
+    rm -rf $C
+    sync
+  ;;
+  *)
+    echo "Usage: $0 {backup|restore}"
+    exit 1
 esac
 
 exit 0
