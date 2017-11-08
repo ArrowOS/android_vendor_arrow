97b1e123aca0177dd7c8f26c2a5811b635ec5749
Tom Marshall <tdm@cyngn.com>
2017-11-07 20:49:43 -0500

    Put install tools in OUT/install
     * This allows us to avoid modifying /system at install time.
    
    Change-Id: I747551d7f38a3eef70ab64f32b6d4a4749c01012

--- build/tools/backuptool.sh
+++ build/tools/backuptool.sh
@@ -6,6 +6,9 @@
 export C=/tmp/backupdir
 export S=/system
 
+# Scripts in /system/addon.d expect to find backuptool.functions in /tmp
+cp -f /tmp/install/bin/backuptool.functions /tmp
+
 # Preserve /system/addon.d in /tmp/addon.d
 preserve_addon_d() {
   mkdir -p /tmp/addon.d/
