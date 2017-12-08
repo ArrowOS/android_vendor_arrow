86a48022d3797230c302c5630c0dd6a1b1076f59
Dan Pasanen <dan.pasanen@gmail.com>
2017-12-08 13:46:30 -0600

    kernel: don't build for TARGET_NO_KERNEL targets
    Change-Id: I14ad45c06922c0775e0574ddbcfcdc33353af465

--- build/tasks/kernel.mk
+++ build/tasks/kernel.mk
@@ -69,6 +69,7 @@
 #   NEED_KERNEL_MODULE_ROOT            = Optional, if true, install kernel
 #                                          modules in root instead of system
 
+ifneq ($(TARGET_NO_KERNEL),true)
 
 TARGET_AUTO_KDIR := $(shell echo $(TARGET_DEVICE_DIR) | sed -e 's/^device/kernel/g')
 
@@ -407,3 +408,5 @@ endif
 
 .PHONY: kernel
 kernel: $(INSTALLED_KERNEL_TARGET)
+
+endif # TARGET_NO_KERNEL
