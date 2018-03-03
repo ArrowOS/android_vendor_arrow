5c7eb54588167dd96408185309e6070747810675
Rashed Abdel-Tawab <rashed@linux.com>
2018-03-03 10:05:13 +0100

    Move all system props to PRODUCT_GENERIC_PROPERTIES
    For Treble enabled devices, PRODUCT_PROPERTY_OVERRIDES puts props
    added with that in /vendor/build.prop. While that's great for
    device-specific properties, these are global Lineage properties
    which are descriptive of the system side, so move them to the list
    that gets populated into /system/build.prop.
    
    Change-Id: Ic9df3e15bc560672dcae886ecfe7bb0a857f59b6

--- config/props.mk
+++ config/props.mk
@@ -13,10 +13,10 @@
 # limitations under the License.
 
 ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)
-PRODUCT_PROPERTY_OVERRIDES += \
+PRODUCT_GENERIC_PROPERTIES += \
     ro.com.google.clientidbase=android-google
 else
-PRODUCT_PROPERTY_OVERRIDES += \
+PRODUCT_GENERIC_PROPERTIES += \
     ro.com.google.clientidbase=$(PRODUCT_GMS_CLIENTID_BASE)
 endif
 
@@ -30,7 +30,7 @@ endif
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
-PRODUCT_PROPERTY_OVERRIDES += \
+PRODUCT_GENERIC_PROPERTIES += \
     keyguard.no_require_sim=true \
     dalvik.vm.debug.alloc=0 \
     ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
@@ -44,14 +44,14 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.wfd.virtual=0
 
 # Media
-PRODUCT_PROPERTY_OVERRIDES += \
+PRODUCT_GENERIC_PROPERTIES += \
     media.recorder.show_manufacturer_and_model=true
 
 # Storage manager
-PRODUCT_PROPERTY_OVERRIDES += \
+PRODUCT_GENERIC_PROPERTIES += \
     ro.storage_manager.enabled=true
 
 # Tethering - allow without requiring a provisioning app
 # (for devices that check this)
-PRODUCT_PROPERTY_OVERRIDES += \
+PRODUCT_GENERIC_PROPERTIES += \
     net.tethering.noprovisioning=true
