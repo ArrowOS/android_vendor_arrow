ea070aaffe1d6e943790ff2acfe34edeaa5ff7a9
Ethan Chen <intervigil@gmail.com>
2018-03-03 18:44:40 -0800

    qcom_target: Move RIL path mapping to core/config.mk
    * hardware/ril is a required project for any build.
      Move the path mapping logic to core/config.mk so
      any build has access to the correct default mapping.
    
    Change-Id: Ia52919d7877ddc91f8a4a988fd0858d14837fc4c

--- build/core/qcom_target.mk
+++ build/core/qcom_target.mk
@@ -3,10 +3,6 @@
 # Bring in Qualcomm helper macros
 include vendor/arrow/build/core/qcom_utils.mk
 
-# Populate the qcom hardware variants in the project pathmap.
-define ril-set-path-variant
-$(call project-set-path-variant,ril,TARGET_RIL_VARIANT,hardware/$(1))
-endef
 define wlan-set-path-variant
 $(call project-set-path-variant,wlan,TARGET_WLAN_VARIANT,hardware/qcom/$(1))
 endef
@@ -97,7 +93,6 @@ $(call set-device-specific-path,LOC_API,loc-api,vendor/qcom/opensource/location)
 $(call set-device-specific-path,DATASERVICES,dataservices,vendor/qcom/opensource/dataservices)
 $(call set-device-specific-path,POWER,power,hardware/qcom/power)
 
-$(call ril-set-path-variant,ril)
 $(call wlan-set-path-variant,wlan-caf)
 $(call bt-vendor-set-path-variant,bt-caf)
 
@@ -113,7 +108,6 @@ $(call project-set-path,qcom-sensors,hardware/qcom/sensors)
 $(call project-set-path,qcom-loc-api,vendor/qcom/opensource/location)
 $(call project-set-path,qcom-dataservices,$(TARGET_DEVICE_DIR)/dataservices)
 
-$(call ril-set-path-variant,ril)
 $(call wlan-set-path-variant,wlan)
 $(call bt-vendor-set-path-variant,bt)
 
