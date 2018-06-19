d757e3b9488c896505e3ecda8f2b83a1ae2a8399
Bauuuuu <abansode@outlook.com>
2018-06-19 16:08:48 +0200

    vendor: changes for Arrow
    * move out the packages
    * cleanup

--- config/packages.mk
+++ config/packages.mk
@@ -1,13 +1,82 @@
+# Copyright (C) 2018 ArrowOS
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
 # Required packages
 PRODUCT_PACKAGES += \
-    Basic \
-    CellBroadcastReceiver \
-    Development \
-    LatinIME
+    BluetoothExt \
+    Calendar \
+    ExactCalculator \
+    LatinIME \
+    Launcher3 \
+    messaging
 
 # Extra tools
 PRODUCT_PACKAGES += \
-    openvpn \
+    7z \
+    bash \
+    bzip2 \
+    curl \
     e2fsck \
+    fsck.ntfs \
+    gdbserver \
+    htop \
+    lib7z \
+    libsepol \
+    micro_bench \
     mke2fs \
-    tune2fs
+    mkfs.ntfs \
+    mount.ntfs \
+    nano \
+    openvpn \
+    oprofiled \
+    pigz \
+    powertop \
+    sqlite3 \
+    strace \
+    tune2fs \
+    unrar \
+    unzip \
+    vim \
+    wget \
+    zip
+
+# Exchange support
+PRODUCT_PACKAGES += \
+    Exchange2
+
+# Openssh
+PRODUCT_PACKAGES += \
+    scp \
+    sftp \
+    ssh \
+    sshd \
+    sshd_config \
+    ssh-keygen \
+    start-ssh
+
+# rsync
+PRODUCT_PACKAGES += \
+    rsync
+
+# Include librsjni explicitly to workaround GMS issue
+PRODUCT_PACKAGES += \
+    librsjni
+
+# These packages are excluded from user builds
+ifneq ($(TARGET_BUILD_VARIANT),user)
+PRODUCT_PACKAGES += \
+    procmem \
+    procrank
+endif
