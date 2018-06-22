namdeptrai2003/android_vendor_dnd

build: core: include platform helper macros in target makefiles
* These are now needed sooner than we had been importing them
  in android_build. This ensures they'll be imported before
  their macros are needed.

Change-Id: Id7e31f36393414f6d51a8bc80686ceaebfe7cb28

build/core/mtk_target.mk
@@ -1,3 +1,6 @@
+# Bring in Mediatek helper macros
+include vendor/cm/build/core/mtk_utils.mk
+
 ifeq ($(BOARD_USES_MTK_HARDWARE),true)
     mtk_flags := -DMTK_HARDWARE
 

build/core/qcom_target.mk
@@ -1,5 +1,8 @@
 # Target-specific configuration
 
+# Bring in Qualcomm helper macros
+include vendor/cm/build/core/qcom_utils.mk
+
 # Populate the qcom hardware variants in the project pathmap.
 define ril-set-path-variant
 $(call project-set-path-variant,ril,TARGET_RIL_VARIANT,hardware/$(1))

ArrowOS/android_vendor_arrow

build/core/mtk_target.mk
@@ -1,3 +1,6 @@
+# Bring in Mediatek helper macros
+include vendor/arrow/build/core/mtk_utils.mk
+
 ifeq ($(BOARD_USES_MTK_HARDWARE),true)
     mtk_flags := -DMTK_HARDWARE
 

build/core/qcom_target.mk
@@ -1,5 +1,8 @@
 # Target-specific configuration
 
+# Bring in Qualcomm helper macros
+include vendor/arrow/build/core/qcom_utils.mk
+
 # Populate the qcom hardware variants in the project pathmap.
 define ril-set-path-variant
 $(call project-set-path-variant,ril,TARGET_RIL_VARIANT,hardware/$(1))