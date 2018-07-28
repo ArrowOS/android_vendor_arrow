8cdfd6ff78953b7540d63f9fd2748394407c7ed6
bauuuuu <abansode@outlook.com>
2018-07-29 00:06:44 +0200

    vendor: fix padding and remove official check
    Change-Id: I41dfd9b58a126711229323e335e04472c7d144da

--- config/version.mk
+++ config/version.mk
@@ -15,32 +15,7 @@
 
 ARROW_MOD_VERSION = v9.0
 
-ifndef ARROW_BUILD_TYPE
-    ARROW_BUILD_TYPE := UNOFFICIAL
-endif
-
-ifeq ($(ARROW_BETA),true)
-    ARROW_BUILD_TYPE := BETA
-endif
-
-CURRENT_DEVICE=$(shell echo "$(TARGET_PRODUCT)" | cut -d'_' -f 2,3)
-
-ifdef ARROW_OFFICIAL
-   LIST = $(shell curl -s https://raw.githubusercontent.com/ArrowOS/android_vendor_arrow/arrow-9.x/arrow.devices)
-   FOUND_DEVICE =  $(filter $(CURRENT_DEVICE), $(LIST))
-    ifeq ($(FOUND_DEVICE),$(CURRENT_DEVICE))
-      IS_OFFICIAL=true
-      ARROW_BUILD_TYPE := OFFICIAL
-
-PRODUCT_PACKAGES += \
-    Updater
-
-    endif
-    ifneq ($(IS_OFFICIAL), true)
-       ARROW_BUILD_TYPE := UNOFFICIAL
-       $(error Device is not official "$(FOUND)")
-    endif
-endif
+ARROW_BUILD_TYPE := OFFICIAL
 
 ARROW_VERSION := Arrow-$(ARROW_MOD_VERSION)-$(CURRENT_DEVICE)-$(ARROW_BUILD_TYPE)-$(shell date -u +%Y%m%d)
 
