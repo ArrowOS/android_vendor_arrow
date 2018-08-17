fa94d51aa2272b3c762c21dbd5275fe71d63a266
Sam Mortimer <sam@mortimer.me.uk>
2018-08-17 14:23:57 -0700

    vendor/arrow: Add support for java source overlays
    *) Set JAVA_SOURCE_OVERLAYS in a device makefile as follows:
       # Java sourcefile overlays (for Android.bp built modules only)
       # Format is a whitespace separated set of rules, each of which
       # structured as follows:
       # modulename|overlay directory|glob pattern within overlay dir
       JAVA_SOURCE_OVERLAYS := \
               org.lineageos.hardware|$(LOCAL_PATH)/lineagehw|**/*.java
    
    Change-Id: I6be1c12567081357f5231a84df98ac002c0563b4

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -37,6 +37,7 @@ type ProductVariables struct {
 	Additional_gralloc_10_usage_bits  *string `json:",omitempty"`
 	Has_legacy_camera_hal1  *bool `json:",omitempty"`
 	Needs_legacy_camera_hal1_dyn_native_handle  *bool `json:",omitempty"`
+	Java_Source_Overlays *string `json:",omitempty"`
 	Needs_text_relocations  *bool `json:",omitempty"`
 	Specific_camera_parameter_library  *string `json:",omitempty"`
 	Target_process_sdk_version_override  *string `json:",omitempty"`
