dfe4ad629860bd926d34e88cdc8b9264bb9214da
Rashed Abdel-Tawab <rashed@linux.com>
2018-08-17 09:20:21 -0700

    lineage: Ignore neverallows
    Change-Id: I35a39ec73c0047f93124f72249a6fa936fe0875a

--- build/core/config.mk
+++ build/core/config.mk
@@ -3,3 +3,6 @@ include $(TOPDIR)vendor/arrow/build/core/mtk_target.mk
 
 # Rules for QCOM targets
 include $(TOPDIR)vendor/arrow/build/core/qcom_target.mk
+
+# We modify several neverallows, so let the build proceed
+SELINUX_IGNORE_NEVERALLOWS := true
