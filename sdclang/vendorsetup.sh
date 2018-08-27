3581b400e926d012e87176b145035e23342a9127
Joe Maples <joe@frap129.org>
2018-08-27 11:59:33 -0400

    sdclang: Add Pie configuration
    * Remove old configuration
    * Add vendorsetup.sh based configuration to be soong compatible
    * Update LTO defs to lastest CAF changes
      * Removed SDCLANG2 references
      * Added back the unsafe filter
      * Re-ordered to minimize the diff
    
    Change-Id: Ia06168f498b217c633d6a0f449715f134fa8a4f4
    Signed-off-by: Joe Maples <joe@frap129.org>

---
+++ sdclang/vendorsetup.sh
@@ -0,0 +1,20 @@
+# Set SDClang defaults
+export SDCLANG=false
+export SDCLANG_PATH=vendor/qcom/sdclang-6.0/linux-x86/bin
+export SDCLANG_LTO_DEFS=vendor/carbon/sdclang/sdllvm-lto-defs.mk
+export SDCLANG_COMMON_FLAGS="-O3 -fvectorize -Wno-user-defined-warnings -Wno-vectorizer-no-neon -Wno-unknown-warning-option \
+-Wno-deprecated-register -Wno-tautological-type-limit-compare -Wno-sign-compare -Wno-gnu-folding-constant \
+-mllvm -arm-implicit-it=always -Wno-inline-asm -Wno-unused-command-line-argument"
+
+# Enable based on host OS/availablitiy
+case $(uname -s) in
+    Linux)
+        if [ -d "$SDCLANG_PATH" ]; then
+            export SDCLANG=true
+        fi
+        ;;
+    Darwin)
+        ;;
+    *)
+        ;;
+esac
