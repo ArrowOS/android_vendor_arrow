3745f0f89dc149a092e7df0c947c45fcf82a3866
Joe Maples <joe@frap129.org>
2018-08-27 11:59:33 -0400

    sdclang: Add Pie configuration
    * Remove old configuration
    * Add vendorsetup.sh based configuration to be soong compatible
    * Update LTO defs to lastest CAF changes
      * Removed SDCLANG2 references
      * Added back the unsafe filter
      * Re-ordered to minimize the diff
    
    Change-Id: Ia06168f498b217c633d6a0f449715f134fa8a4f4
    Signed-off-by: Joe Maples <joe@frap129.org>

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -45,4 +45,5 @@ type ProductVariables struct {
 	Uses_nvidia_enhancements  *bool `json:",omitempty"`
 	Uses_qcom_bsp_legacy  *bool `json:",omitempty"`
 	Uses_qti_camera_device  *bool `json:",omitempty"`
+	Target_use_sdclang							*bool `json:",omitempty"`
 }
