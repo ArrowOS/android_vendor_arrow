958723676d7eb82725c67a68d3dafa4bda3e0b9d
Michael Bestas <mkbestas@lineageos.org>
2018-08-28 20:31:16 +0300

    soong_config: Add flag for crypto waiting on QSEE to start
    Change-Id: I9a5493667c6bb3636468a90df0da65cf5e8c3a8b

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -11,6 +11,9 @@ type Product_variables struct {
 	Device_support_legacy_hwfde struct {
 		Cflags []string
 	}
+	Device_support_wait_for_qsee struct {
+		Cflags []string
+	}
 	Has_legacy_camera_hal1 struct {
 		Cflags []string
 	}
@@ -42,6 +45,7 @@ type ProductVariables struct {
 	Device_support_hwfde  *bool `json:",omitempty"`
 	Device_support_hwfde_perf  *bool `json:",omitempty"`
 	Device_support_legacy_hwfde  *bool `json:",omitempty"`
+	Device_support_wait_for_qsee  *bool `json:",omitempty"`
 	Has_legacy_camera_hal1  *bool `json:",omitempty"`
 	Needs_legacy_camera_hal1_dyn_native_handle  *bool `json:",omitempty"`
 	Needs_text_relocations  *bool `json:",omitempty"`
