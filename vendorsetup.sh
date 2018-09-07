de363ce727e0d54d316e53e33b4d3bf78a7096e7
Bauuuuu <abansode@outlook.com>
2018-09-07 16:56:23 +0200

    vendor: update vendorsetup
    Change-Id: I83d3efb09bc53e8ba32653063de775b114917ea7

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
-for combo in $(curl -s https://raw.githubusercontent.com/ArrowOS/android_vendor_arrow/arrow-8.x/arrow.devices | sed -e 's/#.*$//' | awk '{printf "arrow_%s-%s\n", $1, $2}')
+for combo in $(curl -s https://raw.githubusercontent.com/ArrowOS/android_vendor_arrow/arrow-9.x/arrow.devices | sed -e 's/#.*$//' | awk '{printf "arrow_%s-%s\n", $1, $2}')
 do
     add_lunch_combo $combo
 done
