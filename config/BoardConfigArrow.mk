a4a4b938b36a6952f7bd2056f796eeca115fc18c
Rashed Abdel-Tawab <rashed@linux.com>
2018-09-18 21:53:24 -0700

    arrow: Dynamically generate kernel headers using arrow generator
    Add a soong vendor plugin for kernel config variables so we can use
    these in go.
    
    Change-Id: Id31f2be8fcc5aba2d965dbe815edaaf1d28279c6

--- config/BoardConfigArrow.mk
+++ config/BoardConfigArrow.mk
@@ -3,3 +3,5 @@ include vendor/arrow/config/BoardConfigKernel.mk
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
 include vendor/arrow/config/BoardConfigQcom.mk
 endif
+
+include vendor/arrow/config/BoardConfigSoong.mk
---
+++ config/BoardConfigSoong.mk
@@ -0,0 +1,20 @@
+# Add variables that we wish to make available to soong here.
+EXPORT_TO_SOONG := \
+    TARGET_KERNEL_ARCH \
+    TARGET_KERNEL_CONFIG \
+    TARGET_KERNEL_SOURCE
+
+# Setup SOONG_CONFIG_* vars to export the vars listed above.
+# Documentation here:
+# https://github.com/LineageOS/android_build_soong/commit/8328367c44085b948c003116c0ed74a047237a69
+
+SOONG_CONFIG_NAMESPACES += arrowVarsPlugin
+
+SOONG_CONFIG_arrowVarsPlugin :=
+
+define addVar
+  SOONG_CONFIG_arrowVarsPlugin += $(1)
+  SOONG_CONFIG_arrowVarsPlugin_$(1) := $$($1)
+endef
+
+$(foreach v,$(EXPORT_TO_SOONG),$(eval $(call addVar,$(v))))
