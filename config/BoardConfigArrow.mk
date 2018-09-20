a064caf455a012278c7aafd17d543f5b81f8619d
Rashed Abdel-Tawab <rashed@linux.com>
2018-09-20 15:19:57 -0700

    arrow: Move kernel variable configuration to BoardConfigKernel
    Kernel source settings should always come at BoardConfig population
    time so things that use the variable later don't end up pointing to
    an empty or wrong variable.
    
    The following is also squashed in:
    
    Author: Christopher N. Hesse <raymanfx@gmail.com>
    Date:   Fri Aug 10 00:23:54 2018 +0200
        tasks: kernel: Honor prebuilt kernel flag
        For devices that want to use a prebuilt kernel, TARGET_KERNEL_SOURCE
        would still be set to TARGET_AUTO_KDIR, meaning the build system would
        still try to build the kernel if TARGET_AUTO_KDIR was present.
        Setting TARGET_PREBUILT_KERNEL indicates this is not wanted, so don't
        attempt to do it.
        Change-Id: Ic79b3ac1b9c946fd258ada43dce2b08bb74ea0d9
    
    Change-Id: I7f1607c9cd0dd9f0b37742f8e6520ac2123e18bd

--- config/BoardConfigArrow.mk
+++ config/BoardConfigArrow.mk
@@ -1,22 +1,9 @@
-#
-# Copyright (C) 2018 ArrowOS
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License
-
-# QCOM HW crypto
-ifeq ($(TARGET_HW_DISK_ENCRYPTION),true)
-    TARGET_CRYPTFS_HW_PATH ?= vendor/qcom/opensource/cryptfs_hw
+# Charger
+ifeq ($(WITH_LINEAGE_CHARGER),true)
+    BOARD_HAL_STATIC_LIBRARIES := libhealthd.lineage
 endif
+
+include vendor/arrow/config/BoardConfigKernel.mk
 
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
 include vendor/arrow/config/BoardConfigQcom.mk
