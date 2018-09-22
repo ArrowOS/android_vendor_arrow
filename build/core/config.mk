0fd861d42da61264e88178a35b23761e7f694106
Rashed Abdel-Tawab <rashed@linux.com>
2018-09-21 18:24:01 -0700

    vendor: Move qcom pathmap setting into "BoardConfig"
    Since we're making pathmaps set soong namespaces now, the project
    pathmaps need to be set at BoardConfig reading time. Move them to
    the right place.
    
    Example soong namespace list:
    PRODUCT_SOONG_NAMESPACES= hardware/ril hardware/qcom/audio-caf/msm8998
        hardware/qcom/display-caf/msm8998 hardware/qcom/media-caf/msm8998
        hardware/qcom/camera device/essential/mata/gps hardware/qcom/sensors
        vendor/qcom/opensource/location vendor/qcom/opensource/dataservices
        hardware/qcom/power hardware/qcom/thermal hardware/qcom/vr
        hardware/qcom/wlan-caf hardware/qcom/bt-caf
    
    Change-Id: I718a4d042b555802f769fe931b91e0338e6aee37

--- build/core/config.mk
+++ build/core/config.mk
@@ -1,10 +1,8 @@
 BUILD_RRO_SYSTEM_PACKAGE := $(TOPDIR)vendor/arrow/build/core/system_rro.mk
 
-# Rules for MTK targets
-include $(TOPDIR)vendor/arrow/build/core/mtk_target.mk
-
 # Rules for QCOM targets
 include $(TOPDIR)vendor/arrow/build/core/qcom_target.mk
 
 # We modify several neverallows, so let the build proceed
 SELINUX_IGNORE_NEVERALLOWS := true
+
