f83c5ff5e1950aa08eed412d2631d8b1714676cd
Erfan Abdi <erfangplus@gmail.com>
2019-03-09 12:03:41 +0200

    soong_config: Add flag for devices use metadata as FDE key
    Change-Id: I5f70280e72613a8f850cb3263c45e95e80cb8b3c
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -20,6 +20,7 @@ $(call add_json_bool, Uses_nvidia_enhancements,                   $(filter TRUE,
 $(call add_json_bool, Uses_qcom_bsp_legacy,                       $(filter true,$(TARGET_USES_QCOM_BSP_LEGACY)))
 $(call add_json_bool, Uses_qti_camera_device,                     $(filter true,$(TARGET_USES_QTI_CAMERA_DEVICE)))
 $(call add_json_bool,	Target_use_sdclang,							$(filter true,$(if $(strip $(TARGET_USE_SDCLANG)),true,false)))
+$(call add_json_bool, Uses_metadata_as_fde_key, $(filter true,$(TARGET_USES_METADATA_AS_FDE_KEY)))
 
 # This causes the build system to strip out the last comma in our nested struct, to keep the JSON valid.
 _contents := $(_contents)__SV_END
