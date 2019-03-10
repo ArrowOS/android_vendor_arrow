f38026ef04431a730a77b020cc0651261a1535fb
Bauuuuu <abansode@outlook.com>
2019-03-10 18:56:32 +0100

    vendor: switch to Customized pixel launcher
    http://4pda.ru/forum/index.php?s=&showtopic=903913
    
    Credits:- whataudoinghere
    
    Change-Id: I6b3af64cb220817ccdcf0ec4d49a262444b0d818

---
+++ prebuilt/app/CPixel_Launcher/Android.mk
@@ -0,0 +1,13 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := CPixel_Launcher
+LOCAL_OVERRIDES_PACKAGES := Gallery2 Gallery3D GalleryNew3D
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := APPS
+LOCAL_BUILT_MODULE_STEM := package.apk
+LOCAL_PRIVILEGED_MODULE := true
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_DEX_PREOPT := false
+LOCAL_SRC_FILES := CPixel_Launcher.apk
+include $(BUILD_PREBUILT)
