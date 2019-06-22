a7691872f573824ba237cf026883a42585e9baf3
Ethan Chen <intervigil@gmail.com>
2019-06-22 14:12:40 -0700

    build: Separate platform macros from QCOM platform definition
    * The macros used in the file formerly called "qcom_utils.mk" are
      actually generic and not QCOM-specific. Move the QCOM_BOARD definitions
      to a QCOM-specific location, and name the remaning macros something more
      generic.
    
    Change-Id: I6747bc9201e56b456caf251122057d7dbc233f1c

--- build/core/config.mk
+++ build/core/config.mk
@@ -1,8 +1,5 @@
 BUILD_RRO_SYSTEM_PACKAGE := $(TOPDIR)vendor/arrow/build/core/system_rro.mk
 
-# Rules for MTK targets
-include $(TOPDIR)vendor/arrow/build/core/mtk_target.mk
-
 # Rules for QCOM targets
 include $(TOPDIR)vendor/arrow/build/core/qcom_target.mk
 
