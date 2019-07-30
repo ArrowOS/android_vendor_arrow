1f1fa21d54c896b3f7622bc3b29467ddb7508f91
Bauuuuu <abansode@outlook.com>
2019-07-30 13:57:16 +0200

    core: move arrow sepolicy to vendor [2/3]
    instead of separate repo for arrow sepolicy it will be included in vendor
    Moved from device/arrow/sepolicy repo and also cleanup of unused sepolicy.
    
    Change-Id: I174e0e6c5fbf3a2eefa594142c511abde4906681

---
+++ sepolicy/common/sepolicy.mk
@@ -0,0 +1,25 @@
+#
+# This policy configuration will be used by all products that
+# inherit from arrow
+#
+
+ifeq ($(TARGET_COPY_OUT_VENDOR), vendor)
+ifeq ($(BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE),)
+TARGET_USES_PREBUILT_VENDOR_SEPOLICY ?= true
+endif
+endif
+
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
+    $(TOPDIR)/vendor/arrow/sepolicy/common/public
+
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    $(TOPDIR)/vendor/arrow/sepolicy/common/private
+
+ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    $(TOPDIR)/vendor/arrow/sepolicy/common/dynamic
+else
+BOARD_SEPOLICY_DIRS += \
+    $(TOPDIR)/vendor/arrow/sepolicy/common/dynamic \
+    $(TOPDIR)/vendor/arrow/sepolicy/common/vendor
+endif
