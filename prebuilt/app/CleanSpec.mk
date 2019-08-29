1ebc115c6e3ed9158b31d6e0f046530075fdd6fa
ganeshi4u <ganeshvarmai4u@gmail.com>
2019-08-29 11:43:30 +0200

    arrow: switch browser to chromium
    * screams in mega bytes *
    
    Change-Id: I62d0af924233063ba88c11a1ec8fc92df8b177ef

--- prebuilt/app/CleanSpec.mk
+++ prebuilt/app/CleanSpec.mk
@@ -1,2 +1,2 @@
-$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/app/Browser)
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/app/Chrome)
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/priv-app/stats)
