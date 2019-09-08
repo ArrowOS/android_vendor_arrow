32eb4c4dfe43ec82b2673d81f7b72e4346d177c6
Rashed Abdel-Tawab <rashed@linux.com>
2019-09-07 17:01:13 -0700

    kernel: Use absolute path for kernel O= argument
    The kernel make command is executed in the build root. Without absolute
    out paths, kernel out start with "out/" meaning it would be created in
    the kernel source. Add BUILD_ROOT to the argument to ensure it's always
    made in the actual out/ dir.
    
    Change-Id: I425134a893d4d5c9f31efccaf75e153b5462b76c

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -3,8 +3,7 @@ PATH_OVERRIDE_SOONG := $(shell echo $(TOOLS_PATH_OVERRIDE) | sed -e 's|$$|$$$$|g
 # Add variables that we wish to make available to soong here.
 EXPORT_TO_SOONG := \
     KERNEL_ARCH \
-    KERNEL_CC \
-    KERNEL_CLANG_TRIPLE \
+    KERNEL_BUILD_OUT_PREFIX \
     KERNEL_CROSS_COMPILE \
     KERNEL_MAKE_CMD \
     KERNEL_MAKE_FLAGS \
