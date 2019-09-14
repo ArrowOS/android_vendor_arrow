799ae12fe3d49372eadab4948c4a0b5e070afb73
harsh-23 <harsh.v23labs@gmail.com>
2019-09-14 23:27:08 +0530

    Add Pixel Blue accent
    Change-Id: Ifaa9f74f5fe13b2b672390a74e0ea5f1ec84ac67

--- config/packages.mk
+++ config/packages.mk
@@ -36,6 +36,7 @@ PRODUCT_PACKAGES += \
     AccentColorRedOverlay \
     AccentColorQGreenOverlay \
     AccentColorPinkOverlay \
+    AccentColorPixelBlueOverlay \
     AccentColorLightPurpleOverlay \
     AccentColorIndigoOverlay \
     AccentColorFlatPinkOverlay \
