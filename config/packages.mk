afc6dcaa9e1708cde2af9b0272abc461746d453d
harsh-23 <harsh.v23labs@gmail.com>
2019-09-16 10:41:11 +0200

    overlay: add a few new backgrounds for settings
    Change-Id: I90f518616ea000a409ae4007e43e0145416d6dca

--- config/packages.mk
+++ config/packages.mk
@@ -48,6 +48,15 @@ PRODUCT_PACKAGES += \
     AccentColorCyanOverlay \
     AccentColorBlueGrayOverlay
 
+# Custom Overlays
+# Settings
+PRODUCT_PACKAGES += \
+    PitchBlackOverlay \
+    PitchBlackSettingsExtOverlay \
+    DarkGrayOverlay \
+    StyleOverlay \
+    NightOverlay
+
 # Fonts
 PRODUCT_COPY_FILES += \
     vendor/arrow/fonts/GoogleSans-Regular.ttf:system/fonts/GoogleSans-Regular.ttf \
