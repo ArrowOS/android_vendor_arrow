f90efc456574fabb8de42499f3f239274f8ec74d
ganeshi4u <ganeshvarmai4u@gmail.com>
2019-09-16 14:09:14 +0200

    soong: fix our HW FDE soong vars
    Change-Id: I63d8ba93b7e75f50fc8e57c413b1d456afc9c527

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -6,15 +6,15 @@ type Product_variables struct {
 	Apply_msm8974_1440p_egl_workaround struct {
 		Cflags []string
 	}
-	Device_support_hwfde struct {
+	Supports_hw_fde struct {
 		Cflags []string
 		Header_libs []string
 		Shared_libs []string
 	}
-	Device_support_hwfde_perf struct {
+	Supports_hw_fde_perf struct {
 		Cflags []string
 	}
-	Device_support_legacy_hwfde struct {
+	Supports_legacy_hw_fde struct {
 		Cflags []string
 	}
 	Device_support_wait_for_qsee struct {
@@ -78,9 +78,9 @@ type Product_variables struct {
 
 type ProductVariables struct {
 	Additional_gralloc_10_usage_bits  *string `json:",omitempty"`
-	Device_support_hwfde  *bool `json:",omitempty"`
-	Device_support_hwfde_perf  *bool `json:",omitempty"`
-	Device_support_legacy_hwfde  *bool `json:",omitempty"`
+	Supports_hw_fde  *bool `json:",omitempty"`
+	Supports_hw_fde_perf  *bool `json:",omitempty"`
+	Supports_legacy_hw_fde  *bool `json:",omitempty"`
 	Device_support_wait_for_qsee  *bool `json:",omitempty"`
 	Apply_msm8974_1440p_egl_workaround  *bool `json:",omitempty"`
 	Has_legacy_camera_hal1  *bool `json:",omitempty"`
