c63617f094f5dc60871af1cf341a1d8bbe95ffe3
kubersharma001 <kubersharma001@gmail.com>
2019-12-18 12:50:33 +0530

    vendor: Move fonts part to proper makefile
    Change-Id: I0469a2e7bd965d056f1d6b6a0f790db87811b705

--- config/packages.mk
+++ config/packages.mk
@@ -56,17 +56,6 @@ PRODUCT_PACKAGES += \
     SystemNightOverlay \
     SystemUINightOverlay
 
-# Fonts
-PRODUCT_COPY_FILES += \
-    vendor/arrow/fonts/GoogleSans-Regular.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/GoogleSans-Regular.ttf \
-    vendor/arrow/fonts/GoogleSans-Medium.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/GoogleSans-Medium.ttf \
-    vendor/arrow/fonts/GoogleSans-MediumItalic.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/GoogleSans-MediumItalic.ttf \
-    vendor/arrow/fonts/GoogleSans-Italic.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/GoogleSans-Italic.ttf \
-    vendor/arrow/fonts/GoogleSans-Bold.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/GoogleSans-Bold.ttf \
-    vendor/arrow/fonts/GoogleSans-BoldItalic.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/GoogleSans-BoldItalic.ttf
-
-ADDITIONAL_FONTS_FILE := vendor/arrow/fonts/google-sans.xml
-
 # Extra tools
 PRODUCT_PACKAGES += \
     7z \
