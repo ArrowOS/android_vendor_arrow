ca646988a85b3d70f1a12993563b570c8f6e675b
Kuber Sharma <kubersharma001@gmail.com>
2020-01-30 17:40:06 +0100

    Revert "Revert "vendor: remove global selinux neverallow flag""
    This reverts commit 2e6c9892385b85c239fab0528313d3be968f43ce.
    
    Change-Id: I53103aa79557b0fc7986129b9eb813d79c57377c

--- build/core/config.mk
+++ build/core/config.mk
@@ -4,4 +4,4 @@ BUILD_RRO_SYSTEM_PACKAGE := $(TOPDIR)vendor/arrow/build/core/system_rro.mk
 include $(TOPDIR)vendor/arrow/build/core/qcom_target.mk
 
 # We modify several neverallows, so let the build proceed
-SELINUX_IGNORE_NEVERALLOWS := true
+#SELINUX_IGNORE_NEVERALLOWS := true
