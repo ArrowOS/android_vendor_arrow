d28f1f883815b43dbc906cb2564d7db9ffb3d0ad
Han Wang <416810799@qq.com>
2020-03-10 09:40:47 +0200

    extract_utils: Fix up "Fix bad substitution for zsh"
    Change-Id: I9e5ea799812ca932de95ec6cbdedb604cbde5736

--- build/tools/extract_utils.sh
+++ build/tools/extract_utils.sh
@@ -1198,11 +1198,11 @@ function oat2dex() {
     fi
 
     if [ -z "$VDEXEXTRACTOR" ]; then
-        export VDEXEXTRACTOR="$ARROW_ROOT"/prebuilts/tools-extras/{HOST}-x86/bin/vdexExtractor
+        export VDEXEXTRACTOR="$ARROW_ROOT"/prebuilts/tools-extras/${HOST}-x86/bin/vdexExtractor
     fi
 
     if [ -z "$CDEXCONVERTER" ]; then
-        export CDEXCONVERTER="$ARROW_ROOT"/prebuilts/tools-extras/{HOST}-x86/bin/compact_dex_converter
+        export CDEXCONVERTER="$ARROW_ROOT"/prebuilts/tools-extras/${HOST}-x86/bin/compact_dex_converter
     fi
 
     # Extract existing boot.oats to the temp folder
