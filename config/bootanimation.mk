4f272e5b1c9a73d870a91b7670fd1981314aedbb
JaxTheWolf <roman.lubij@gmail.com>
2020-03-16 14:22:49 +0100

    vendor: bootanim: Update|add for 1080p, 720p and 480p devices
    Change-Id: I1f1b411b6895d80abde7e1e77374377fd4d128f9

--- config/bootanimation.mk
+++ config/bootanimation.mk
@@ -1,2 +1,15 @@
-PRODUCT_COPY_FILES += \
-    vendor/arrow/prebuilt/common/bootanimation/bootanimation.zip:system/media/bootanimation.zip
+# Boot Animation
+
+# 1080p
+ifeq ($(TARGET_BOOT_ANIMATION_RES),1080)
+PRODUCT_COPY_FILES += vendor/arrow/prebuilt/common/bootanimation/1080.zip:$(TARGET_COPY_OUT_SYSTEM)/media/bootanimation.zip
+# 720p
+else ifeq ($(TARGET_BOOT_ANIMATION_RES),720)
+PRODUCT_COPY_FILES += vendor/arrow/prebuilt/common/bootanimation/720.zip:$(TARGET_COPY_OUT_SYSTEM)/media/bootanimation.zip
+# 480p
+else ifeq ($(TARGET_BOOT_ANIMATION_RES),480)
+PRODUCT_COPY_FILES += vendor/arrow/prebuilt/common/bootanimation/480.zip:$(TARGET_COPY_OUT_SYSTEM)/media/bootanimation.zip
+# Default to 720p if the device does not set the flag.
+else
+PRODUCT_COPY_FILES += vendor/arrow/prebuilt/common/bootanimation/720.zip:$(TARGET_COPY_OUT_SYSTEM)/media/bootanimation.zip
+endif
