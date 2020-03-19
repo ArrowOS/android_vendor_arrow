LineageOS/android_vendor_lineage

default-permissions: Grant default permissions to com.android.exchange
Change-Id: I9dfcecde0e662b9e481696b661e16ac7e131a75c

config/common.mk
@@ -87,6 +87,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/lineage/config/permissions/lineage-power-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/lineage-power-whitelist.xml
 
+# Pre-granted permissions
+PRODUCT_COPY_FILES += \
+    vendor/lineage/config/permissions/lineage-default-permissions.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default-permissions/lineage-default-permissions.xml
+
 # Include AOSP audio files
 include vendor/lineage/config/aosp_audio.mk
 

ArrowOS/android_vendor_arrow

config/common.mk
@@ -70,6 +70,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/arrow/prebuilt/common/etc/sysconfig/custom-power-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/custom-power-whitelist.xml
 
+# Pre-granted permissions
+PRODUCT_COPY_FILES += \
+    vendor/arrow/config/permissions/arrow-default-permissions.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/default-permissions/arrow-default-permissions.xml
+
 # Permissions
 PRODUCT_COPY_FILES += \
     vendor/arrow/config/permissions/privapp-permissions-arrow.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-arrow.xml