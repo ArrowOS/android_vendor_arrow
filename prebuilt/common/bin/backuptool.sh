e3a6fa57b7af25b20dfd157f73876cb531a8ea85
kubersharma001 <kubersharma001@gmail.com>
2020-03-19 19:18:40 +0530

    vendor: Sync backuptool from LineageOS
     * Till 8f3d941706809717e1be01028d97d41064ddcc55
    
    Change-Id: If7338753cbdf3c63eb464c512da99b771e0ea7c4

--- prebuilt/common/bin/backuptool.sh
+++ prebuilt/common/bin/backuptool.sh
@@ -1,23 +1,24 @@
 #!/sbin/sh
 #
-# Backup and restore addon /$S files
+# Backup and restore addon /system files
 #
 
 export C=/tmp/backupdir
 export S=$2
+export V=v10.0
 
 export ADDOND_VERSION=1
 
-# Scripts in /$S/addon.d expect to find backuptool.functions in /tmp
+# Scripts in /system/addon.d expect to find backuptool.functions in /tmp
 cp -f /tmp/install/bin/backuptool.functions /tmp
 
-# Preserve /$S/addon.d in /tmp/addon.d
+# Preserve /system/addon.d in /tmp/addon.d
 preserve_addon_d() {
   if [ -d $S/addon.d/ ]; then
     mkdir -p /tmp/addon.d/
     cp -a $S/addon.d/* /tmp/addon.d/
 
-  # Discard any scripts that aren't at least our version level
+    # Discard any scripts that aren't at least our version level
     for f in /postinstall/tmp/addon.d/*sh; do
       SCRIPT_VERSION=$(grep "^# ADDOND_VERSION=" $f | cut -d= -f2)
       if [ -z "$SCRIPT_VERSION" ]; then
@@ -32,7 +33,7 @@ preserve_addon_d() {
   fi
 }
 
-# Restore /$S/addon.d in /tmp/addon.d
+# Restore /system/addon.d from /tmp/addon.d
 restore_addon_d() {
   if [ -d /tmp/addon.d/ ]; then
     mkdir -p $S/addon.d/
@@ -41,7 +42,50 @@ restore_addon_d() {
   fi
 }
 
-# Execute /$S/addon.d/*.sh scripts with $1 parameter
+# Proceed only if /system is the expected major and minor version
+check_prereq() {
+# If there is no build.prop file the partition is probably empty.
+if [ ! -r $S/build.prop ]; then
+    return 0
+fi
+if ! grep -q "^ro.modversion=$V.*" $S/build.prop; then
+  echo "Not backing up files from incompatible version: $V"
+  return 0
+fi
+return 1
+}
+
+check_blacklist() {
+  if [ -f $S/addon.d/blacklist -a -d /$1/addon.d/ ]; then
+      ## Discard any known bad backup scripts
+      cd /$1/addon.d/
+      for f in *sh; do
+          [ -f $f ] || continue
+          s=$(md5sum $f | cut -c-32)
+          grep -q $s $S/addon.d/blacklist && rm -f $f
+      done
+  fi
+}
+
+check_whitelist() {
+  found=0
+  if [ -f $S/addon.d/whitelist ];then
+      ## forcefully keep any version-independent stuff
+      cd /$1/addon.d/
+      for f in *sh; do
+          s=$(md5sum $f | cut -c-32)
+          grep -q $s $S/addon.d/whitelist
+          if [ $? -eq 0 ]; then
+              found=1
+          else
+              rm -f $f
+          fi
+      done
+  fi
+  return $found
+}
+
+# Execute /system/addon.d/*.sh scripts with $1 parameter
 run_stage() {
 if [ -d /tmp/addon.d/ ]; then
   for script in $(find /tmp/addon.d/ -name '*.sh' |sort -n); do
@@ -53,12 +97,24 @@ fi
 case "$1" in
   backup)
     mkdir -p $C
+    if check_prereq; then
+        if check_whitelist system; then
+            exit 127
+        fi
+    fi
+    check_blacklist system
     preserve_addon_d
     run_stage pre-backup
     run_stage backup
     run_stage post-backup
   ;;
   restore)
+    if check_prereq; then
+        if check_whitelist tmp; then
+            exit 127
+        fi
+    fi
+    check_blacklist tmp
     run_stage pre-restore
     run_stage restore
     run_stage post-restore
