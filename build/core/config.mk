1f534057048fc9efa8ae595decb4639a07a1d977
kubersharma001 <kubersharma001@gmail.com>
2020-04-10 11:55:48 +0200

    arrow: Add our custom flag for allowing selinux neverallows
     * Suitable for both user and debug builds
    
    Change-Id: I65879d9e60009f654ac0939941cdfe2f55b10c69

--- build/core/config.mk
+++ build/core/config.mk
@@ -1,5 +1,13 @@
 # Rules for QCOM targets
 include $(TOPDIR)vendor/arrow/build/core/qcom_target.mk
 
+# BIG HAX MOMENT
 # We modify several neverallows, so let the build proceed
-#SELINUX_IGNORE_NEVERALLOWS := true
+# Even for user build type devices with neverallows
+ifeq ($(TARGET_ALLOW_NEVERALLOWS), true)
+    ifeq ($(TARGET_BUILD_VARIANT),user)
+        SELINUX_IGNORE_NEVERALLOWS_ON_USER := true
+    else
+        SELINUX_IGNORE_NEVERALLOWS := true
+    endif
+endif
