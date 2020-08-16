cac3e550f5cba76fb04ff0636357baab9ca3e402
palaych <palaychm@yandex.ru>
2020-08-16 06:56:28 +0000

    vendor: Remove qcom um soong and add back qcom platform type to soongs vars
    Include revert a3d4954e72c97fae1dcdbcfcc4699085d2507707
    
    Co-authored-by: jhenrique09 <jhenrique09.mcz@hotmail.com>
    Change-Id: Iaf044c3e6c6ff7d04b20cea50df1673f2e862b79

--- config/BoardConfigQcom.mk
+++ config/BoardConfigQcom.mk
@@ -102,3 +102,16 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/qcom/audio-caf/$(QCOM_HARDWARE_VARIANT) \
     hardware/qcom/display-caf/$(QCOM_HARDWARE_VARIANT) \
     hardware/qcom/media-caf/$(QCOM_HARDWARE_VARIANT)
+
+# Required for frameworks/native
+ifeq ($(QCOM_HARDWARE_VARIANT),msm8996)
+    TARGET_USES_QCOM_UM_3_18_FAMILY := true
+else ifeq ($(QCOM_HARDWARE_VARIANT),msm8998)
+    TARGET_USES_QCOM_UM_4_4_FAMILY := true
+else ifeq ($(QCOM_HARDWARE_VARIANT),sdm845)
+    TARGET_USES_QCOM_UM_4_9_FAMILY := true
+else ifeq ($(QCOM_HARDWARE_VARIANT),sm8150)
+    TARGET_USES_QCOM_UM_4_14_FAMILY := true
+else ifeq ($(QCOM_HARDWARE_VARIANT),sm8250)
+    TARGET_USES_QCOM_UM_4_19_FAMILY := true
+endif
