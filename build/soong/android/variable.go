f487d859f5ec9dc9d3c86d59e8e169d5894c515e
kubersharma001 <kubersharma001@gmail.com>
2020-09-14 21:16:37 +0530

    vendor: Cleanup, reorder and sync soong changes from lineage
    * https://github.com/LineageOS/android_vendor_lineage/commits/0409f2311910fb6532210ccf434e8610344a8f41
    
    Change-Id: I348f853f476152a3d3f9ee4a641262d0ef503051

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -6,18 +6,7 @@ type Product_variables struct {
 	Apply_msm8974_1440p_egl_workaround struct {
 		Cflags []string
 	}
-	Supports_hw_fde struct {
-		Cflags []string
-		Header_libs []string
-		Shared_libs []string
-	}
-	Supports_hw_fde_perf struct {
-		Cflags []string
-	}
-	Should_wait_for_qsee struct {
-		Cflags []string
-	}
-	Supports_extended_compress_format struct {
+	Bootloader_message_offset struct {
 		Cflags []string
 	}
 	Has_legacy_camera_hal1 struct {
@@ -29,14 +18,31 @@ type Product_variables struct {
 		Cflags []string
 		Header_libs []string
 	}
-	Needs_legacy_camera_hal1_dyn_native_handle struct {
-		Cppflags []string
+	Recovery_skip_ev_rel_input struct {
+		Cflags []string
 	}
-	Needs_text_relocations struct {
+	Should_wait_for_qsee struct {
+		Cflags []string
+	}
+	Supports_extended_compress_format struct {
+		Cflags []string
+	}
+	Supports_hw_fde struct {
+		Cflags []string
+		Header_libs []string
+		Shared_libs []string
+	}
+	Supports_hw_fde_perf struct {
+		Cflags []string
+	}
+	Target_ignores_ftp_pptp_conntrack_failure struct {
 		Cppflags []string
 	}
 	Target_init_vendor_lib struct {
 		Whole_static_libs []string
+	}
+	Target_needs_netd_direct_connect_rule struct {
+		Cppflags []string
 	}
 	Target_process_sdk_version_override struct {
 		Cppflags []string
@@ -44,12 +50,6 @@ type Product_variables struct {
 	Target_shim_libs struct {
 		Cppflags []string
 	}
-	Target_needs_netd_direct_connect_rule struct {
-		Cppflags []string
-	}
-	Target_ignores_ftp_pptp_conntrack_failure struct {
-		Cppflags []string
-        }
 	Target_surfaceflinger_fod_lib struct {
 		Cflags []string
 		Whole_static_libs []string
@@ -71,25 +71,24 @@ type Product_variables struct {
 
 type ProductVariables struct {
 	Additional_gralloc_10_usage_bits  *string `json:",omitempty"`
-	Supports_hw_fde  *bool `json:",omitempty"`
-	Supports_hw_fde_perf  *bool `json:",omitempty"`
-	Should_wait_for_qsee  *bool `json:",omitempty"`
 	Apply_msm8974_1440p_egl_workaround  *bool `json:",omitempty"`
+	Bootloader_message_offset  *int `json:",omitempty"`
 	Has_legacy_camera_hal1  *bool `json:",omitempty"`
 	Qcom_um_soong_namespace  *string `json:",omitempty"`
-	Needs_legacy_camera_hal1_dyn_native_handle  *bool `json:",omitempty"`
-	Needs_text_relocations  *bool `json:",omitempty"`
+	Recovery_skip_ev_rel_input  *bool `json:",omitempty"`
+	Should_wait_for_qsee  *bool `json:",omitempty"`
 	Specific_camera_parameter_library  *string `json:",omitempty"`
 	Supports_extended_compress_format  *bool `json:",omitempty"`
+	Supports_hw_fde  *bool `json:",omitempty"`
+	Supports_hw_fde_perf  *bool `json:",omitempty"`
+	Target_ignores_ftp_pptp_conntrack_failure  *bool `json:",omitempty"`
 	Target_init_vendor_lib  *string `json:",omitempty"`
+	Target_needs_netd_direct_connect_rule  *bool `json:",omitempty"`
 	Target_process_sdk_version_override  *string `json:",omitempty"`
 	Target_shim_libs  *string `json:",omitempty"`
-	Target_needs_netd_direct_connect_rule  *bool `json:",omitempty"`
-	Target_ignores_ftp_pptp_conntrack_failure  *bool `json:",omitempty"`
 	Target_surfaceflinger_fod_lib  *string `json:",omitempty"`
 	Uses_generic_camera_parameter_library  *bool `json:",omitempty"`
 	Uses_nvidia_enhancements  *bool `json:",omitempty"`
 	Uses_qcom_bsp_legacy  *bool `json:",omitempty"`
 	Uses_qti_camera_device  *bool `json:",omitempty"`
-	Target_use_sdclang  *bool `json:",omitempty"`
 }
