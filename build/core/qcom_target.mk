80d53be2c97c2a26a8e0677e2def923e374382a7
Bauuuuu <abansode@outlook.com>
2020-09-23 11:36:39 +0200

    vendor: bring back option to use aosp wlan
    Change-Id: I70259165177a3b848ae008b48a60dc1336a2bcd6

--- build/core/qcom_target.mk
+++ build/core/qcom_target.mk
@@ -19,7 +19,12 @@ $(call set-device-specific-path,DATA_IPA_CFG_MGR,data-ipa-cfg-mgr,vendor/qcom/op
 $(call set-device-specific-path,DATASERVICES,dataservices,vendor/qcom/opensource/dataservices)
 $(call set-device-specific-path,THERMAL,thermal,hardware/qcom-caf/thermal)
 $(call set-device-specific-path,VR,vr,hardware/qcom-caf/vr)
+
+ifeq ($(BOARD_USES_AOSP_WLAN_HAL),true)
+$(call set-device-specific-path,WLAN,wlan,hardware/qcom/wlan)
+else
 $(call set-device-specific-path,WLAN,wlan,hardware/qcom-caf/wlan)
+endif
 
 PRODUCT_CFI_INCLUDE_PATHS += \
     hardware/qcom-caf/wlan/qcwcn/wpa_supplicant_8_lib
