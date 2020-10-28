f58ce8ec44916ed915407605e74998d73589319f
LuK1337 <priv.luk@gmail.com>
2020-10-28 13:04:08 +0100

    soong: Add TARGET_USES_NO_MTU_IPACM
    Change-Id: Ibb9559a4d01ca79848323474db0130281bcf04a6

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -49,6 +49,7 @@ SOONG_CONFIG_arrowQcomVars += \
     supports_extended_compress_format \
     supports_hw_fde \
     supports_hw_fde_perf \
+    uses_no_mtu_ipacm \
     uses_pre_uplink_features_netmgrd \
     uses_qcom_bsp_legacy \
     uses_qti_camera_device
@@ -68,6 +69,7 @@ SOONG_CONFIG_arrowQcomVars_should_wait_for_qsee := $(TARGET_KEYMASTER_WAIT_FOR_Q
 SOONG_CONFIG_arrowQcomVars_supports_extended_compress_format := $(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)
 SOONG_CONFIG_arrowQcomVars_supports_hw_fde := $(TARGET_HW_DISK_ENCRYPTION)
 SOONG_CONFIG_arrowQcomVars_supports_hw_fde_perf := $(TARGET_HW_DISK_ENCRYPTION_PERF)
+SOONG_CONFIG_arrowQcomVars_uses_no_mtu_ipacm := $(TARGET_USES_NO_MTU_IPACM)
 SOONG_CONFIG_arrowQcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD)
 SOONG_CONFIG_arrowQcomVars_uses_qcom_bsp_legacy := $(TARGET_USES_QCOM_BSP_LEGACY)
 SOONG_CONFIG_arrowQcomVars_uses_qti_camera_device := $(TARGET_USES_QTI_CAMERA_DEVICE)
