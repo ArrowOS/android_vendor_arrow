Havoc-OS/android_vendor_havoc

vendor: soong: Add camera_needs_client_info
Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

config/BoardConfigSoong.mk
@@ -31,6 +31,7 @@ SOONG_CONFIG_NAMESPACES += havocGlobalVars
 SOONG_CONFIG_havocGlobalVars += \
     additional_gralloc_10_usage_bits \
     bootloader_message_offset \
+    camera_needs_client_info \
     disable_postrender_cleanup \
     has_legacy_camera_hal1 \
     has_memfd_backport \
@@ -67,6 +68,7 @@ SOONG_CONFIG_havocQcomVars += \
 endif
 
 # Soong bool variables
+SOONG_CONFIG_havocGlobalVars_camera_needs_client_info := $(TARGET_CAMERA_NEEDS_CLIENT_INFO)
 SOONG_CONFIG_havocGlobalVars_disable_postrender_cleanup := $(TARGET_DISABLE_POSTRENDER_CLEANUP)
 SOONG_CONFIG_havocGlobalVars_has_legacy_camera_hal1 := $(TARGET_HAS_LEGACY_CAMERA_HAL1)
 SOONG_CONFIG_havocGlobalVars_has_memfd_backport := $(TARGET_HAS_MEMFD_BACKPORT)

ArrowOS/android_vendor_arrow

Change-Id: I60a9b2a137d70e7592b43dd2f9a05bbb6a2ef694

config/BoardConfigSoong.mk
@@ -30,6 +30,7 @@ SOONG_CONFIG_NAMESPACES += arrowGlobalVars
 SOONG_CONFIG_arrowGlobalVars += \
     additional_gralloc_10_usage_bits \
     bootloader_message_offset \
+    camera_needs_client_info \
     disable_postrender_cleanup \
     has_legacy_camera_hal1 \
     ignores_ftp_pptp_conntrack_failure \
@@ -63,6 +64,7 @@ SOONG_CONFIG_arrowQcomVars += \
 endif
 
 # Soong bool variables
+SOONG_CONFIG_arrowGlobalVars_camera_needs_client_info := $(TARGET_CAMERA_NEEDS_CLIENT_INFO)
 SOONG_CONFIG_arrowGlobalVars_disable_postrender_cleanup := $(TARGET_DISABLE_POSTRENDER_CLEANUP)
 SOONG_CONFIG_arrowGlobalVars_has_legacy_camera_hal1 := $(TARGET_HAS_LEGACY_CAMERA_HAL1)
 SOONG_CONFIG_arrowGlobalVars_ignores_ftp_pptp_conntrack_failure := $(TARGET_IGNORES_FTP_PPTP_CONNTRACK_FAILURE)