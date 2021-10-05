8f3b27e35d1a748686f5fb193f1512f586dd7b87
kubersharma001 <kubersharma001@gmail.com>
2021-10-05 09:15:01 +0000

    [DO NOT MERGE][TEMPORARY] Include GApps only if exists
    Change-Id: I9b4e2f7ebd456fe4bb6a0a4bc2656910677f3a8d

--- config/version.mk
+++ config/version.mk
@@ -22,6 +22,7 @@ ifeq ($(ARROW_BETA),true)
 endif
 
 ifeq ($(ARROW_GAPPS), true)
+    $(call inherit-product-if-exists, vendor/gapps/common/common-vendor.mk)
     ARROW_BUILD_ZIP_TYPE := GAPPS
 endif
 
