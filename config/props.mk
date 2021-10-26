55df9d2f05fed76d56d310d8366b6f138649e79f
kubersharma001 <kubersharma001@gmail.com>
2021-10-26 13:38:06 +0000

    arrow: props: Disable blur on App Launch
    Ref: https://android.googlesource.com/platform/packages/apps/Launcher3/+/refs/tags/android-12.0.0_r3/quickstep/src/com/android/launcher3/statehandlers/DepthController.java#232
    
    Change-Id: Iab609dc9be5fbb8ccc5e124420a214e261fdc931

--- config/props.mk
+++ config/props.mk
@@ -33,6 +33,10 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.build.selinux=1 \
     persist.sys.disable_rescue=true
 
+# Blur
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.launcher.blur.appLaunch=false
+
 # Media
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     media.recorder.show_manufacturer_and_model=true
