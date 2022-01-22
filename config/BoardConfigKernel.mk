004b748fd321369627816af002768b0a644ccca3
Dakkshesh07 <dakkshesh5@gmail.com>
2022-01-22 20:43:00 +0530

    kernel: allow custom clang support for CLANG_PREBUILTS
    Signed-off-by: Dakkshesh07 <dakkshesh5@gmail.com>
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    Change-Id: If1cfb7ab558e8554e9cc20108c42cfdb5b1ad0e6

--- config/BoardConfigKernel.mk
+++ config/BoardConfigKernel.mk
@@ -71,6 +71,20 @@ KERNEL_PATCHLEVEL := $(shell grep -s "^PATCHLEVEL = " $(TARGET_KERNEL_SOURCE)/Ma
 TARGET_KERNEL_VERSION ?= $(shell echo $(KERNEL_VERSION)"."$(KERNEL_PATCHLEVEL))
 
 CLANG_PREBUILTS := $(BUILD_TOP)/prebuilts/clang/host/$(HOST_PREBUILT_TAG)/clang-r450784d
+ifneq ($(TARGET_CLANG_PREBUILTS_VERSION),)
+    ifeq ($(TARGET_CLANG_PREBUILTS_VERSION),latest)
+        # Set the latest version of clang
+        CLANG_PREBUILTS_VERSION := $(shell ls -d $(BUILD_TOP)/prebuilts/clang/host/$(HOST_OS)-x86/clang-r* | xargs -n 1 basename | tail -1)
+    else
+        # Find the clang-* directory containing the specified version
+        CLANG_PREBUILTS_VERSION := clang-$(TARGET_CLANG_PREBUILTS_VERSION)
+    endif
+else
+    # Use the default version of clang if TARGET_CLANG_PREBUILTS_VERSION hasn't been set by the device config
+    CLANG_PREBUILTS_VERSION := clang-r416183b1
+endif
+
+CLANG_PREBUILTS := $(BUILD_TOP)/prebuilts/clang/host/$(HOST_PREBUILT_TAG)/$(CLANG_PREBUILTS_VERSION)
 GCC_PREBUILTS := $(BUILD_TOP)/prebuilts/gcc/$(HOST_PREBUILT_TAG)
 
 # x86 toolchain
