38a73c9ce6632e74c9e64e94d0305a5758734cf5
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-05-18 09:51:13 +0200

    soong: Drop TARGET_USES_QTI_CAMERA_DEVICE
    * qcom-camera topic hasn't been ported to 19.1
    
    Change-Id: Ifb053d215527d602109bbb451d96ec80cf028548

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -42,7 +42,6 @@ SOONG_CONFIG_NAMESPACES += arrowQcomVars
 SOONG_CONFIG_arrowQcomVars += \
     supports_extended_compress_format \
     uses_pre_uplink_features_netmgrd \
-    uses_qti_camera_device \
     needs_camera_boottime_timestamp
 
 # Only create display_headers_namespace var if dealing with UM platforms to avoid breaking build for all other platforms
@@ -55,7 +54,6 @@ endif
 SOONG_CONFIG_arrowNvidiaVars_uses_nvidia_enhancements := $(NV_ANDROID_FRAMEWORK_ENHANCEMENTS)
 SOONG_CONFIG_arrowQcomVars_supports_extended_compress_format := $(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)
 SOONG_CONFIG_arrowQcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD)
-SOONG_CONFIG_arrowQcomVars_uses_qti_camera_device := $(TARGET_USES_QTI_CAMERA_DEVICE)
 SOONG_CONFIG_arrowQcomVars_needs_camera_boottime_timestamp := $(TARGET_CAMERA_BOOTTIME_TIMESTAMP)
 
 # Set default values
