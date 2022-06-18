17d0614b1e22fe82d4dd03cd57c383be7de67372
Sarthak Roy <sarthakroy2002@gmail.com>
2022-06-18 03:23:33 +0000

    arrow: fonts: Import HarmonyOS Sans
    * Huawei's licensing on HarmonyOS Sans is looking quite good for us to
    shipping it.
    * Font weight config value from HentaiOS
    
    Co-authored-by: Raphiel Rollerscaperers <rapherion@raphielgang.org>
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>
    Change-Id: I2f09726e5fda34c667d6b0e2cfc64fdb0818ab6c

--- config/fonts.mk
+++ config/fonts.mk
@@ -23,7 +23,19 @@ PRODUCT_COPY_FILES += \
     vendor/arrow/fonts/GoogleSans-Italic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Italic.ttf \
     vendor/arrow/fonts/GoogleSans-Regular.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Regular.ttf \
     vendor/arrow/fonts/Inter-VF.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/Inter-VF.ttf \
-    vendor/arrow/fonts/OppoSans-En-Regular.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/OppoSans-En-Regular.ttf
+    vendor/arrow/fonts/OppoSans-En-Regular.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/OppoSans-En-Regular.ttf \
+    vendor/arrow/fonts/HarmonyOS_Sans_Black_Italic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/HarmonyOS_Sans_Black_Italic.ttf \
+    vendor/arrow/fonts/HarmonyOS_Sans_Black.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/HarmonyOS_Sans_Black.ttf \
+    vendor/arrow/fonts/HarmonyOS_Sans_Bold_Italic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/HarmonyOS_Sans_Bold_Italic.ttf \
+    vendor/arrow/fonts/HarmonyOS_Sans_Bold.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/HarmonyOS_Sans_Bold.ttf \
+    vendor/arrow/fonts/HarmonyOS_Sans_Light_Italic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/HarmonyOS_Sans_Light_Italic.ttf \
+    vendor/arrow/fonts/HarmonyOS_Sans_Light.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/HarmonyOS_Sans_Light.ttf \
+    vendor/arrow/fonts/HarmonyOS_Sans_Medium_Italic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/HarmonyOS_Sans_Medium_Italic.ttf \
+    vendor/arrow/fonts/HarmonyOS_Sans_Medium.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/HarmonyOS_Sans_Medium.ttf \
+    vendor/arrow/fonts/HarmonyOS_Sans_Regular_Italic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/HarmonyOS_Sans_Regular_Italic.ttf \
+    vendor/arrow/fonts/HarmonyOS_Sans_Regular.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/HarmonyOS_Sans_Regular.ttf \
+    vendor/arrow/fonts/HarmonyOS_Sans_Thin_Italic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/HarmonyOS_Sans_Thin_Italic.ttf \
+    vendor/arrow/fonts/HarmonyOS_Sans_Thin.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/HarmonyOS_Sans_Thin.ttf
 
 PRODUCT_PACKAGES += \
     FontArbutusSourceOverlay \
@@ -32,4 +44,5 @@ PRODUCT_PACKAGES += \
     FontGoogleSansLatoOverlay \
     FontInterOverlay \
     FontOppoSansOverlay \
+    FontHarmonyOS-SansOverlay \
     FontRubikRubikOverlay
