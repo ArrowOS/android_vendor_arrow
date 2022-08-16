adb3f77662e50efbf5382ecde2a8e9eb726e8fa0
Kujou Yuko <i@779.moe>
2022-08-16 09:01:46 +0000

    generator: Don't care android.SourceDepTag
     * android.SourceDepTag is deprecated.
    
    Error: vendor/arrow/build/soong/generator/generator.go:150:17: undefined: android.SourceDepTag.
    
    Signed-off-by: Kujou Yuko <i@779.moe>
    Change-Id: Idf9709f67848a9b704919eee1273f05d6ac9dfb8

--- build/soong/generator/generator.go
+++ build/soong/generator/generator.go
@@ -147,8 +147,6 @@ func (g *Module) GenerateAndroidBuildActions(ctx android.ModuleContext) {
 	if len(g.properties.Tools) > 0 {
 		ctx.VisitDirectDepsBlueprint(func(module blueprint.Module) {
 			switch ctx.OtherModuleDependencyTag(module) {
-			case android.SourceDepTag:
-				// Nothing to do
 			case hostToolDepTag:
 				tool := ctx.OtherModuleName(module)
 				var path android.OptionalPath
