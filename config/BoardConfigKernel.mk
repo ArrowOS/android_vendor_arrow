cd8fad7a08b1bfb22d32bca54c45bfe821c35199
Kujou Yuko <i@779.moe>
2022-08-16 09:17:40 +0000

    config: kernel: Update clang path to clang-r450784d
    Signed-off-by: Kujou Yuko <i@779.moe>
    Change-Id: Ia04d95d85d61d05e06513438da71932c07aaa4ea

--- config/BoardConfigKernel.mk
+++ config/BoardConfigKernel.mk
@@ -50,7 +50,7 @@ else
 KERNEL_ARCH := $(TARGET_KERNEL_ARCH)
 endif
 
-CLANG_PREBUILTS := $(BUILD_TOP)/prebuilts/clang/host/$(HOST_PREBUILT_TAG)/clang-r416183b1
+CLANG_PREBUILTS := $(BUILD_TOP)/prebuilts/clang/host/$(HOST_PREBUILT_TAG)/clang-r450784d
 GCC_PREBUILTS := $(BUILD_TOP)/prebuilts/gcc/$(HOST_PREBUILT_TAG)
 
 # x86 toolchain
