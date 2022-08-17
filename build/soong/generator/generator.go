bc1b448c582aa118c461d8f929922a31d31013eb
Arian <arian.kulmer@web.de>
2022-08-17 19:28:53 +0200

    soong: generator: [partial-pick] Replace android.SourceDepTag usage with IsSourceDepTagWithOutputTag
    Change-Id: Iad5d9a07d06a7efda233cc8d3387e8db5c3b0ab5

--- build/soong/generator/generator.go
+++ build/soong/generator/generator.go
@@ -184,7 +184,9 @@ func (g *Module) GenerateAndroidBuildActions(ctx android.ModuleContext) {
 					ctx.ModuleErrorf("host tool %q missing output file", tool)
 				}
 			default:
-				ctx.ModuleErrorf("unknown dependency on %q", ctx.OtherModuleName(module))
+				if !android.IsSourceDepTagWithOutputTag(ctx.OtherModuleDependencyTag(module), "") {
+					ctx.ModuleErrorf("unknown dependency on %q", ctx.OtherModuleName(module))
+				}
 			}
 		})
 	}
