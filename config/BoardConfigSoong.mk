28b7b6902a39156ebbebeec78d922dad487aa231
Michael Bestas <mkbestas@lineageos.org>
2022-08-30 02:43:52 +0300

    config: Remove TARGET_HAS_LEGACY_CAMERA_HAL1
    Change-Id: I6f4cbe41f6df3ceb6b4058d5b6e77c02e1899809

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -31,7 +31,6 @@ SOONG_CONFIG_arrowGlobalVars += \
     additional_gralloc_10_usage_bits \
     bootloader_message_offset \
     disable_postrender_cleanup \
-    has_legacy_camera_hal1 \
     ignores_ftp_pptp_conntrack_failure \
     needs_netd_direct_connect_rule \
     target_init_vendor_lib \
@@ -60,7 +59,6 @@ endif
 
 # Soong bool variables
 SOONG_CONFIG_arrowGlobalVars_disable_postrender_cleanup := $(TARGET_DISABLE_POSTRENDER_CLEANUP)
-SOONG_CONFIG_arrowGlobalVars_has_legacy_camera_hal1 := $(TARGET_HAS_LEGACY_CAMERA_HAL1)
 SOONG_CONFIG_arrowGlobalVars_ignores_ftp_pptp_conntrack_failure := $(TARGET_IGNORES_FTP_PPTP_CONNTRACK_FAILURE)
 SOONG_CONFIG_arrowGlobalVars_needs_netd_direct_connect_rule := $(TARGET_NEEDS_NETD_DIRECT_CONNECT_RULE)
 SOONG_CONFIG_arrowNvidiaVars_uses_nvidia_enhancements := $(NV_ANDROID_FRAMEWORK_ENHANCEMENTS)
