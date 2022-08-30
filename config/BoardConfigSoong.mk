fba7d5cc7b1fe0a4b4b2ab2bf2314b83f0054678
Michael Bestas <mkbestas@lineageos.org>
2022-08-30 03:13:55 +0300

    config: Remove TARGET_DISABLE_POSTRENDER_CLEANUP
    This hasn't been ported after lineage-18.1
    
    Change-Id: Ib1bca251c6de7529c58ea2fb9b439e37b55be6c5

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -30,7 +30,6 @@ SOONG_CONFIG_NAMESPACES += arrowGlobalVars
 SOONG_CONFIG_arrowGlobalVars += \
     additional_gralloc_10_usage_bits \
     bootloader_message_offset \
-    disable_postrender_cleanup \
     needs_netd_direct_connect_rule \
     target_init_vendor_lib \
     target_inputdispatcher_skip_event_key \
@@ -56,7 +55,6 @@ SOONG_CONFIG_arrowQcomVars += \
 endif
 
 # Soong bool variables
-SOONG_CONFIG_arrowGlobalVars_disable_postrender_cleanup := $(TARGET_DISABLE_POSTRENDER_CLEANUP)
 SOONG_CONFIG_arrowGlobalVars_needs_netd_direct_connect_rule := $(TARGET_NEEDS_NETD_DIRECT_CONNECT_RULE)
 SOONG_CONFIG_arrowNvidiaVars_uses_nvidia_enhancements := $(NV_ANDROID_FRAMEWORK_ENHANCEMENTS)
 SOONG_CONFIG_arrowQcomVars_supports_extended_compress_format := $(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)
