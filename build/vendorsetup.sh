8524743a9daacac1003e6cb453a9708b9fb27495
Kshitij Gupta <kshitijgm@gmail.com>
2023-03-30 16:16:30 +0530

    vendorsetup: Add function to push to gerrit
    [ghostrider-reborn]:
    - cleanup and rework for arrowos
    - add code-review, verified, private and wip options
    
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Change-Id: Ibe8305ee3d38b2526c389bbf0fd67cb4ee556e62

--- build/vendorsetup.sh
+++ build/vendorsetup.sh
@@ -19,6 +19,61 @@ function repopick() {
     $T/vendor/arrow/build/tools/repopick.py $@
 }
 
+function gerritpush() {
+    local GERRIT_URL=review.arrowos.net
+    local MANIFEST_PATH="$ANDROID_BUILD_TOP/manifest/arrow.xml"
+    local PREFIX="ArrowOS/"
+    local branch=arrow-13.1
+    local ref=for
+    local options="%"
+
+    while [ "$#" -gt 0 ]; do
+      case "$1" in
+        -t)
+            options+="topic=$2,"; shift
+            ;;
+        -c)
+            options+="l=Code-Review$2,"; shift
+            ;;
+        -v)
+            options+="l=Verified$2,"; shift
+            ;;
+        -p)
+            options+="private,"
+            ;;
+        -w)
+            options+="wip,"
+            ;;
+        -d)
+            ref=heads
+            ;;
+        -b)
+            branch="$2"; shift
+            ;;
+      esac
+      shift
+    done
+    if ! git rev-parse --is-inside-work-tree &> /dev/null; then
+        echo "fatal: not a git repository"
+        return 1
+    fi
+    local repo=$(git rev-parse --show-toplevel | sed "s|$ANDROID_BUILD_TOP/||g")
+    local name=$(grep "path=\"$repo\"" $MANIFEST_PATH | sed -e 's/.*name="//' -e 's/".*//')
+    if [[ -z $name ]]; then
+        name=$(grep "name=\"$repo\"" $MANIFEST_PATH | sed -e 's/.*name="//' -e 's/".*//')
+    fi
+    if [[ -z $name ]]; then
+        echo "fatal: repo is not in arrow manifest"
+        return 1
+    fi
+    if [[ -z $ARROW_GERRIT_USER ]]; then
+      printf 'Enter gerrit username: '
+      read -r ARROW_GERRIT_USER
+    fi
+    export ARROW_GERRIT_USER
+    git push "ssh://$ARROW_GERRIT_USER@$GERRIT_URL:29418/$PREFIX$name" "HEAD:refs/$ref/$branch$options"
+}
+
 if [ -z ${CCACHE_EXEC} ]; then
     ccache_path=$(which ccache)
     if [ ! -z "$ccache_path" ]; then
