93c24499cca7dd5217175ae5554bf89763cb2649
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-06-01 21:40:18 +0530

    common: Switch to LMO's face unlock implementation
    Shoutout and thanks to LibreMobileOS for their OSS face unlock
    implementation!
    
    https://github.com/LMODroid/platform_packages_apps_FaceUnlock
    https://github.com/LMODroid/platform_external_yifan
    
    Change-Id: Ib1042d40b3d44fbde6eda8d6635fcb720e4b8d4b

--- config/common.mk
+++ config/common.mk
@@ -44,12 +44,12 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.software.sip.voip.xml
 
 # Face Unlock
-TARGET_FACE_UNLOCK_SUPPORTED ?= false
+TARGET_FACE_UNLOCK_SUPPORTED ?= true
+
 ifeq ($(TARGET_FACE_UNLOCK_SUPPORTED),true)
 PRODUCT_PACKAGES += \
-    FaceUnlockService
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.face_unlock_service.enabled=$(TARGET_FACE_UNLOCK_SUPPORTED)
+    LMOFaceUnlock
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
 endif
