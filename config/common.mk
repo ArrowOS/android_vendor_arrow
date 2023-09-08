4f691ad90131418ece8a00403bb15e10d2647b6f
Pranaya Deomani <pranayadeomani@proton.me>
2023-09-08 10:54:50 +0530

    common: Inherit custom vendor if exists
    Change-Id: I5d3b84f17db04dd144f6ce3d7bc338d78ff17b10

--- config/common.mk
+++ config/common.mk
@@ -12,6 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Allow vendor/extra to override any property by setting it first
+$(call inherit-product-if-exists, vendor/extra/product.mk)
+
 include vendor/arrow/config/version.mk
 
 PRODUCT_PACKAGE_OVERLAYS += \
